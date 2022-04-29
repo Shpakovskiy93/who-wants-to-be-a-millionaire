5704f8724091eb10dd55916251b41da0cf37c6eb
Shpakovskiy93 <mikhail.shpakovskiy.93@gmail.com>
2022-04-29 11:53:39 +0300

    add functions win level and lost game

--- js/main.js
+++ js/main.js
@@ -21,10 +21,15 @@ const levelMoneyEl = document.querySelectorAll('.level__list-item');
 const questionEl = document.querySelector('.question__text');
 const ansersEl = document.querySelectorAll('.anser');
 const timerEl = document.querySelector('.timer');
+const lostEl = document.querySelector('.lost');
+const winGameEl = document.querySelector('.win__game');
 
 
 let startAudio = new Audio('../music/start.mp3');
 let timerAudio = new Audio('../music/timer.mp3');
+let winAnserAudio = new Audio('../music/win-anser.mp3');
+let lostAudio = new Audio('../music/lost.mp3');
+let endGame = new Audio('../music/end-game.mp3');
 
 let level = 0;
 let timer;
@@ -60,23 +65,76 @@ function createCardGame() {
     })
 }
 
+function winLevel(anser) {
+
+    timerAudio.pause();
+    timerAudio.currentTime = 0;
+    timerEl.classList.remove('show');
+
+    setTimeout(() => {
+        ansersEl.forEach(elem => elem.classList.remove('choice__anser'))
+        anser.classList.add('right__anser');
+        winAnserAudio.play();
+    }, 3000);
+
+    setTimeout(() => {
+        winAnserAudio.pause();
+        winAnserAudio.currentTime = 0;
+        levelMoneyEl.forEach(elem => elem.classList.remove('list__item-active'));
+        levelMoneyEl[level].classList.add('list__item-active');
+        level++;
+        ansersEl.forEach(anser => anser.classList.remove('show'));
+        anser.classList.remove('right__anser');
+        game();
+    }, 6000);
+}
+function lostGame(anser) {
+    timerAudio.pause();
+    timerAudio.currentTime = 0;
+    timerEl.classList.remove('show');
+
+    setTimeout(() => {
+        ansersEl.forEach(elem => elem.classList.remove('choice__anser'))
+        anser.classList.add('wrong__anser');
+        lostAudio.play();
+    }, 3000);
+
+    setTimeout(() => {
+        lostAudio.pause();
+        lostAudio.currentTime = 0;
+        levelMoneyEl.forEach(elem => elem.classList.remove('list__item-active'));
+        level = 0;
+        ansersEl.forEach(anser => anser.classList.remove('show'));
+        anser.classList.remove('wrong__anser');
+
+        document.querySelector('.millionaire__inner').classList.remove('show');
+        document.querySelector('.millionaire__question').classList.remove('show');
+        startGameEl.classList.remove('hide');
+    }, 6000);
+}
+
 function getAndComparisonAnser() {
+    this.classList.add('choice__anser');
     let userAnser = this.innerHTML;
     
     if(userAnser == questions[level].anser) {
-        level++;
-        ansersEl.forEach(anser => anser.classList.remove('show'));
-        timerEl.classList.remove('show');
-        timerAudio.pause();
-        timerAudio.currentTime = 0;
-        game();
+        winLevel(this);
+    } else {
+        lostGame(this);
     }
 }
 
 function game() {
-    createCardGame();
 
-    ansersEl.forEach(btn => btn.addEventListener('click', getAndComparisonAnser));
+    if(level == 2) {
+        document.querySelector('.millionaire__inner').classList.remove('show');
+        document.querySelector('.millionaire__question').classList.remove('show');
+        winGameEl.classList.add('show');
+        endGame.play();
+    } else {
+        createCardGame();
+        ansersEl.forEach(btn => btn.addEventListener('click', getAndComparisonAnser));
+    }
 }
 
 startGameEl.addEventListener('click', showGame);
